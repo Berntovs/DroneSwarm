f2ebef174face46d1daf80928d5364321c14a836
Henrik Nøsen Dahle <hnd007@hotmail.com>
2019-05-27 19:28:39 +0200

    Update m_min_max.h

--- Modules/include/m_min_max.h
+++ Modules/include/m_min_max.h
@@ -6,10 +6,22 @@
 
 float check_lower_upper_range(float value, float lower, float upper);
 
-// Returns minimal value
-
+/**
+ * @brief returns the lower value of the two
+ *
+ * @param[in] a  number
+ * @param[in] b  number
+ *
+ * @return uint32_t the lower value
+ */
 uint32_t min(uint32_t a, uint32_t b);
 
-// Returns maximum value
-
+/**
+ * @brief returns the higher value of the two
+ *
+ * @param[in] a  number
+ * @param[in] b  number
+ *
+ * @return uint32_t the higher value
+ */
 uint32_t max(uint32_t a, uint32_t b);
